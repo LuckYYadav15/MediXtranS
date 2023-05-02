9929c031e1360a20201b0eaaf51cfdee2c584ead
LuckYYadav15 <20bcs077@iiitdwd.ac.in>
2023-05-02 06:31:22 +0530

    processed transcription with full text

--- src/pages/Transcription.jsx
+++ src/pages/Transcription.jsx
@@ -47,67 +47,154 @@ function MyComponent() {
       console.error(error);
     }
   };
+  const getEntityColor = (entity_group) => {
+    switch (entity_group) {
+      case "Detailed_description":
+        return "red";
+      case "age":
+        return "yellow";
+      case "history":
+        return "blue";
+      case "nonbiological_location":
+        return "violet";
 
-  const outputList = output.map((result, index) => (
-    <div key={index}>
-      <p>
-        {result.map(({ word, score, entity_group }) => {
-          let color = "black";
-          switch (entity_group) {
-            case "Detailed_description":
-              color = "red";
-              break;
-            case "age":
-              color = "yellow";
-              break;
-            case "history":
-              color = "blue";
-              break;
-            case "nonbiological_location":
-              color = "violet";
-              break;
-            case "Disease_disorder":
-              color = "green";
-              break;
-            case "Diagnostic_procedure":
-              color = "orange";
-              break;
-            case "Lab_value":
-              color = "brown";
-              break;
-            case "Distance":
-              color = "gray";
-              break;
-            case "Clinical_event":
-              color = "aqua";
-              break;
-            case "Sign_symptom":
-              color = "Brown";
-              break;
-            case "Biological_structure":
-              color = "lime";
-              break;
-            case "Frequency":
-              color = "Maroon";
-              break;
-            default:
-              color = "black";
-          }
-          return (
-            <span
-              key={word}
-              style={{
-                color: score >= 0.5 ? color : color,
-                fontSize: score >= 0.5 && entity_group ? "80%" : "100%",
-              }}
-            >
-              {word} {score >= 0.5 && entity_group ? `(${entity_group})` : null}
-            </span>
-          );
-        })}
-      </p>
-    </div>
-  ));
+      case "Disease_disorder":
+        return "green";
+
+      case "Diagnostic_procedure":
+        return "orange";
+
+      case "Lab_value":
+        return "brown";
+
+      case "Distance":
+        return "gray";
+
+      case "Clinical_event":
+        return "aqua";
+
+      case "Sign_symptom":
+        return "Brown";
+
+      case "Biological_structure":
+        return "lime";
+
+      case "Frequency":
+        return "Maroon";
+
+      default:
+        return "black";
+    }
+  };
+
+  const outputList = output.map((result, index) => {
+    const highlightedText = [];
+    let lastIndex = 0;
+    console.log(result.length);
+    console.log(inputs)
+    for (let i = 0; i < result.length; i++) {
+      const { start, end, score, entity_group } = result[i];
+      console.log(result[i]);
+      const color = getEntityColor(entity_group);
+      const textChunk = inputs.substring(lastIndex, start);
+      console.log(textChunk)
+      const entityChunk = inputs.substring(start, end);
+      console.log(entityChunk)
+      if (textChunk) {
+        highlightedText.push(<span key={lastIndex}>{textChunk}</span>);
+      }
+
+      highlightedText.push(
+        <span
+          key={start + "-" + end}
+          style={{
+            color: "black",
+            fontSize: score >= 0.5 && entity_group ? "110%" : "100%",
+            backgroundColor: score >= 0.5 ? color : "transparent",
+          }}
+        >
+          {entityChunk} <b>{entity_group} </b>
+        </span>
+      );
+      lastIndex = end;
+    }
+
+    if (lastIndex < intext.length) {
+      highlightedText.push(
+        <span key={lastIndex}>{intext.substring(lastIndex)}</span>
+      );
+    }
+
+    // console.log(highlightedText[0]);
+
+    return (
+      <div key={index}>
+        <p>{highlightedText}</p>
+      </div>
+    );
+  });
+
+  // const outputList = output.map((result, index) => (
+  //   <div key={index}>
+  //     <p>
+  //       {result.map(({ word, score, entity_group }) => {
+  //         let color = "black";
+  //         switch (entity_group) {
+  //           case "Detailed_description":
+  //             color = "red";
+  //             break;
+  //           case "age":
+  //             color = "yellow";
+  //             break;
+  //           case "history":
+  //             color = "blue";
+  //             break;
+  //           case "nonbiological_location":
+  //             color = "violet";
+  //             break;
+  //           case "Disease_disorder":
+  //             color = "green";
+  //             break;
+  //           case "Diagnostic_procedure":
+  //             color = "orange";
+  //             break;
+  //           case "Lab_value":
+  //             color = "brown";
+  //             break;
+  //           case "Distance":
+  //             color = "gray";
+  //             break;
+  //           case "Clinical_event":
+  //             color = "aqua";
+  //             break;
+  //           case "Sign_symptom":
+  //             color = "Brown";
+  //             break;
+  //           case "Biological_structure":
+  //             color = "lime";
+  //             break;
+  //           case "Frequency":
+  //             color = "Maroon";
+  //             break;
+  //           default:
+  //             color = "black";
+  //         }
+  //         return (
+  //           <span
+  //             key={word}
+  //             style={{
+  //               color: score >= 0.5 ? color : color,
+  //               fontSize: score >= 0.5 && entity_group ? "80%" : "100%",
+  //             }}
+  //           >
+  //             {word} {score >= 0.5 && entity_group ? `(${entity_group})` : null}
+  //           </span>
+  //         );
+  //       })}
+  //     </p>
+  //   </div>
+  // ));
+
   if (loading) {
     // show loading animation if loading state is true
     return <LoadingPage />;
@@ -136,7 +223,7 @@ function MyComponent() {
         </form>
       </div>
       <div style={{ width: "50%" }}>
-        <ul>{outputList}</ul>
+        {outputList}
       </div>
     </div>
   );
