7b12711c5369d928a9dda797edc68d10e3fbba5e
LuckYYadav15 <20bcs077@iiitdwd.ac.in>
2023-05-05 22:38:19 +0530

    login page updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { useCookies } from "react-cookie";
 import { ToastContainer, toast } from "react-toastify";
 import LoadingPage from "../components/Loading/loading";
 import { FastForward } from "react-bootstrap-icons";
+import "./Login.css";
 
 function Login() {
   const [cookies] = useCookies([]);
@@ -51,37 +52,50 @@ function Login() {
     return <LoadingPage />;
   }
   return (
-    <div style={{ width: "400px" }} className="container">
-      <h2>Login to your Account</h2>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={(e) =>
-              setValues({ ...values, [e.target.name]: e.target.value })
-            }
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={(e) =>
-              setValues({ ...values, [e.target.name]: e.target.value })
-            }
-          />
-        </div>
-        <button type="submit">Submit</button>
-        <span>
-          Don't have an account ?<Link to="/register"> Register </Link>
-        </span>
-      </form>
-      <ToastContainer />
+      <div className="row-container">
+        <img
+          style={{
+            width: "100%",
+            height: "400%",
+            marginLeft: "-150px",
+            marginTop: "40px",
+          }}
+          src={process.env.PUBLIC_URL + "/mediXlogo.png"}
+          alt="My Image"
+        />
+        <div style={{ width: "400px" }} className="login-container">
+          <h2>Login to your Account</h2>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+              />
+            </div>
+            <button type="submit">Submit</button>
+            <span>
+              Don't have an account ?<Link to="/register"> Register </Link>
+            </span>
+          </form>
+          <ToastContainer />
+        
+      </div>
     </div>
   );
 }
